fa4a415bbb463bfe9ad3a58b72496285d4784016
mauricio8989 <mauriciosouza8989@gmail.com>
2024-08-08 19:13:53 -0300

    :tada: Commit inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home(){
+    return (
+        <div>
+            <h1>Mãe,se Deus me desse outra chance de viver outra vida, eu so queria se tivesse Voçê!</h1>
+        </div>
+    )
+}
+
+export default Home;
