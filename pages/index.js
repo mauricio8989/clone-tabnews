8a1ae7c2ae54b8276c3c4854ea7e165f801b79fd
mauricio8989 <mauriciosouza8989@gmail.com>
2024-08-11 16:56:29 -0300

    :ok_hand: adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-function Home(){
-    return (
-        <div>
-            <h1>Mãe, se Deus me desse uma  chance de viver outra vez, eu so queria se tivesse Voçê! 💟</h1>
-        </div>
-    )
+function Home() {
+  return (
+    <div>
+      <h1>
+        Mãe, se Deus me desse uma chance de viver outra vez, eu so queria se
+        tivesse Voçê! 💟
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
