95bdadcb966c5edf3c1243fe2a1bf8da4c0b533a
mauricio89 <mauriciosouza8989@gmail.com>
2025-02-09 15:08:29 -0300

    :recycle: refactor: make datbase.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -2,16 +2,21 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    user: process.env.DATABASE_USER,
-    database: process.env.DATABASE_DB,
-    password: process.env.DATABASE_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  client.end();
-  return result;
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error("Error executing query", error.stack);
+  } finally {
+    client.end();
+  }
 }
 
 export default {
