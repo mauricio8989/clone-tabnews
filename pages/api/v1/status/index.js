312606b0a0995f98207ee78421a452dc814be0db
mauricio89 <mauriciosouza8989@gmail.com>
2025-02-09 15:09:05 -0300

    :sparkles: feat: finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,24 @@
 import database from "infra/database";
-
 async function status(req, res, next) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result);
-  res
-    .status(200)
-    .json({ chave: "Alunos do curso.dev são alunos acima da média" });
+  const version = await database.query("SHOW server_version;");
+  const maxConnection = await database.query("SHOW max_connections;");
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const usedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  res.status(200).json({
+    dependencies: {
+      database: {
+        version: version.rows[0].server_version,
+        maxconnection: parseInt(maxConnection.rows[0].max_connections),
+        usedconnections: usedConnections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
