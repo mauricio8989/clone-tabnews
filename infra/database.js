f1dd1b4e8019a7d0795d1c773f6579164aaaa12a
mauricio89 <mauriciosouza8989@gmail.com>
2025-02-11 18:08:00 -0300

    :sparkles: feat: add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,10 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: {
-      rejectUnauthorized: false,
-      ca: process.env.CA_DB,
-    },
+    ssl: getSSLValues(),
   });
   try {
     await client.connect();
@@ -27,3 +24,12 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "development" ? true : false;
+}
